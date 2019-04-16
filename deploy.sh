412d079d692c2f401104f37b17f967709e391ce1
sahir <sahir.bhatnagar@gmail.com>
2019-04-16 00:52:19 -0400

    Publishing source of blog to master branch

--- deploy.sh
+++ deploy.sh
@@ -22,8 +22,7 @@ fi
 echo "Deleting old publication"
 # rm -rf public
 # mkdir public
-git worktree prune
-rm -rf .git/worktrees/public/
+# rm -rf .git/worktrees/public/
 # 
 # echo "Checking out gh-pages branch into public"
 # git worktree add -B gh-pages public origin/gh-pages
@@ -44,6 +43,7 @@ echo "Pushing to gh-pages branch"
 git push origin gh-pages
 
 echo "Cleaning up files"
+git worktree prune
 cd ..
 
 rm -rf public/*
