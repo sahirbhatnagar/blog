cd441cdefd428e9004acf423e29e1f03e7810eb2
sahir <sahir.bhatnagar@gmail.com>
2019-04-16 00:11:02 -0400

    Publishing to master branch

--- deploy.sh
+++ deploy.sh
@@ -23,23 +23,28 @@ fi
 
 #DEPLOY_BRANCH="master"
 
-echo "Deleting old publication"
-rm -rf public
-mkdir public
-git worktree prune
-rm -rf .git/worktrees/public/
+# echo "Deleting old publication"
+# rm -rf public
+# mkdir public
+# git worktree prune
+# rm -rf .git/worktrees/public/
+# 
+# echo "Checking out gh-pages branch into public"
+# git worktree add -B gh-pages public origin/gh-pages
+# 
+# echo "Removing existing files"
+# rm -rf public
 
-echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public origin/gh-pages
-
-echo "Removing existing files"
-rm -rf public
 
 echo "Generating site"
 Rscript -e "blogdown::build_site()"
+# Rscript -e "blogdown::hugo_build()"
 
 echo "Adding and committing to gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish_to_ghpages.sh)" 
+git subtree push --prefix public origin gh-pages
 
-echo "Pushing to gh-pages branch"
-git push origin gh-pages
+# echo "Adding and committing to gh-pages branch"
+# cd public && git add --all && git commit -m "Publishing to gh-pages (publish_to_ghpages.sh)" 
+# 
+# echo "Pushing to gh-pages branch"
+# git push origin gh-pages
